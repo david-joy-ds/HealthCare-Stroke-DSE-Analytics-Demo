8b6b303296b19020443121f2ddbe09f0b53bb270
= <=>
2020-06-10 12:38:21 -0500

    Modified code for Parquet file generation.

--- src/main/scala/HealthPack/SparkHealthApp.scala
+++ src/main/scala/HealthPack/SparkHealthApp.scala
@@ -5,6 +5,7 @@ import com.datastax.spark.connector._
 import org.apache.spark.sql.cassandra._
 import MLApp.mlflow
 import Optimize.optimizeApp
+import java.io._
 /* import Optimize.dsExlpore */
 
 object SparkHealthApp extends App {
@@ -16,7 +17,27 @@ object SparkHealthApp extends App {
 
     /* testing data rdd */
     val rdd = ss.sparkContext.cassandraTable("keyspace1","health_stroke")
-    println("Rdd count : "+rdd.count)
+    println("Rdd count : "+rdd.count+"\n")
+
+    println("Writing to health.parquet file ... \n")
+
+    /* writing data to df */
+    val dff = ss.sqlContext
+      .read
+      .format("org.apache.spark.sql.cassandra")
+      .options(Map( "table" -> "health_stroke", "keyspace" -> "keyspace1"))
+      .load()
+
+    println("df count ... \n"+dff.count())
+
+    dff.write.parquet("file:///tmp/health.parquet")
+
+
+    /* added this below 4 line code to check run as with spark */
+    println("Writing to a File now .....\n")
+    val writer = new PrintWriter(new File("test.txt"))
+    writer.write("Run as Test, I am not broken")
+    writer.close()
 
     /* setting up data for ML flow */
     val data = ss.read.cassandraFormat("health_stroke","keyspace1").load()
--- src/main/scala/Optimize/optimizeApp.scala
+++ src/main/scala/Optimize/optimizeApp.scala
@@ -9,12 +9,12 @@ class optimizeApp {
     /* InEfficient Shuffling */
     val purchasesRDD = ss.sparkContext.parallelize(List((1, 20), (1, 30), (1, 40), (2, 44), (2, 72), (3, 20), (4, 20), (4, 30), (4, 23), (5, 20), (5, 30), (5, 40), (5, 120), (5, 20), (1, 56)))
     val purchaseByCustomer = purchasesRDD.groupByKey().map(f => (f._1, (f._2.size, f._2.sum))).collect()
-    purchaseByCustomer.foreach(f => println("Customer ID " + f._1 + ":" + " Size :" + f._2._1 + " Purchase :" + f._2._2))
+    purchaseByCustomer.foreach(f => println("\n Customer ID " + f._1 + ":" + " Size :" + f._2._1 + " Purchase :" + f._2._2))
 
     /* Efficient Shuffling */
     val purchasesRDD1 = ss.sparkContext.parallelize(List((1, 20), (1, 30), (1, 40), (2, 44), (2, 72), (3, 20), (4, 20), (4, 30), (4, 23), (5, 20), (5, 30), (5, 40), (5, 120), (5, 20), (1, 56)))
     val purchaseByCustomer1 = purchasesRDD1.map(f => (f._1, (1, f._2))).reduceByKey((v1, v2) => (v1._1 + v2._1, v1._2 + v2._2)).collect()
-    purchaseByCustomer1.foreach(f => println("Customer ID " + f._1 + ":" + " Size :" + f._2._1 + " Purchase :" + f._2._2))
+    purchaseByCustomer1.foreach(f => println("\n Customer ID " + f._1 + ":" + " Size :" + f._2._1 + " Purchase :" + f._2._2))
   }
 }
 
